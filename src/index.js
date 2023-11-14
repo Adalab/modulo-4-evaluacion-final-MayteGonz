b8d5786824ddb2c40636f79b91aa9a5d813a49c4
Mayte <mayte710@hotmail.com>
2023-11-14 13:53:41 +0000

    template

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+// create and config server
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+// init express aplication
+const serverPort = 4001;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// conexion con la BD
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
+  });
+  await connection.connect();
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+  );
+
+  return connection;
+}
+// creamos  nuestros endpoints
+//get, post, put, delete
