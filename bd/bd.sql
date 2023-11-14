0444e89d65344f7c78cf27bd1565fdbb249fe758
Mayte <mayte710@hotmail.com>
2023-11-14 17:12:19 +0000

    CRUD

---
+++ bd/bd.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
+--
+-- Host: sql.freedb.tech    Database: freedb_HPcharacters
+-- ------------------------------------------------------
+-- Server version	8.0.35-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `casas`
+--
+
+DROP TABLE IF EXISTS `casas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `casas` (
+  `idCasa` int NOT NULL AUTO_INCREMENT,
+  `nombreCasa` varchar(50) DEFAULT NULL,
+  `fantasma` varchar(50) DEFAULT NULL,
+  `fundador` varchar(50) DEFAULT NULL,
+  `reliquia` varchar(50) DEFAULT NULL,
+  `animal` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idCasa`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `casas`
+--
+
+LOCK TABLES `casas` WRITE;
+/*!40000 ALTER TABLE `casas` DISABLE KEYS */;
+INSERT INTO `casas` VALUES (1,'Hufflepuff','Fraile gordo','Helga Hufflepuff','Copa de Hufflepuff.',' tejón'),(2,'Gryffindor','Nick Casi Decapitado','Godric Griffindor ','Espada de Godric Gryffindor',' león'),(3,'Ravenclaw','Dama Gris','Rowena Ravenclaw','La diadema de Rowena Ravenclaw',' águila'),(4,'Slytherin','Barón Sanguinario','Salazar Slytherin ','Guardapelo de Salazar Slytherin',' serpiente');
+/*!40000 ALTER TABLE `casas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personajes`
+--
+
+DROP TABLE IF EXISTS `personajes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personajes` (
+  `idPersonaje` int NOT NULL AUTO_INCREMENT,
+  `nombrePj` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `cumpleaños` date DEFAULT NULL,
+  `ocupacion` varchar(50) DEFAULT NULL,
+  `boggart` varchar(50) DEFAULT NULL,
+  `fkCasa` int NOT NULL,
+  PRIMARY KEY (`idPersonaje`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personajes`
+--
+
+LOCK TABLES `personajes` WRITE;
+/*!40000 ALTER TABLE `personajes` DISABLE KEYS */;
+INSERT INTO `personajes` VALUES (1,'Hermione','Granger','1979-09-19','Estudiante','Profesora McGonagall diciendole que ha suspendido ',2),(2,'Ginevra','Weasley','1981-08-11','Estudiante','Ser controlada por Lord Voldemord',2),(3,'Nymphadora','Tonks','1973-08-23','Aurora','Perder sus habilidades',1),(4,'Pomona','Sprout','1942-05-15','Profesora','Lord Voldemort',1),(5,'Bellatrix','Lestrange','1951-05-02','Mortífaga','Voldemort rechazandola',4),(6,'Narcisa','Malfoy','1955-01-03','Ama de casa','Perder a su hijo',4),(7,'Luna','Lovegood','1981-02-13','Estudiante','Ver morir a su padre',3),(8,'Cho','Chang','1979-09-01','Estudiante','Ver a Cedric recriminandola ',3);
+/*!40000 ALTER TABLE `personajes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-14 16:16:46
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // create and config server
 const server = express();
@@ -28,5 +29,117 @@ async function getConnection() {
 
   return connection;
 }
+
 // creamos  nuestros endpoints
 //get, post, put, delete
+
+//endpoint de todos los personajes
+server.get('/personajes', async (req, res) => {
+  const connection = await getConnection();
+  const query = 'SELECT * FROM personajes';
+  const [results, fields] = await connection.query(query);
+  connection.end();
+  res.json(results);
+});
+//endpoint de todas las casas
+server.get('/casas', async (req, res) => {
+  const connection = await getConnection();
+  const query = 'SELECT * FROM casas';
+  const [results, fields] = await connection.query(query);
+  connection.end();
+  res.json(results);
+});
+//endpoint de la relación de personajes con casas, donde se muestra solo el nombre y la casa a la que pertenece.
+server.get('/', async (req, res) => {
+  const connection = await getConnection();
+  const query =
+    'SELECT personajes.nombrePj as nombre, personajes.apellido,casas.nombreCasa as casa FROM casas INNER JOIN personajes ON idCasa = fkCasa';
+  const [results, fields] = await connection.query(query);
+  connection.end();
+  res.json(results);
+});
+
+//endpoint para añadir un nuevo personaje
+server.post('/personajes', async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const query =
+      'INSERT INTO personajes (nombrePj, apellido, cumpleaños,ocupacion,boggart,fkCasa) VALUES (?,?,?,?,?,?);';
+
+    const [results] = await connection.query(query, [
+      req.body.nombrePj,
+      req.body.apellido,
+      req.body.cumpleaños,
+      req.body.ocupacion,
+      req.body.boggart,
+      req.body.fkCasa,
+    ]);
+    console.log(results);
+    console.log(results.insertId);
+    connection.end();
+    res.json({
+      success: true,
+      id: results.insertId,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message:
+        'Compruba que no te has dejado por rellenar ningún campo de tu maga favorita o inténtalo de nuevo más tarde',
+    });
+  }
+});
+
+//modificar un personaje
+server.put('/personajes/:idPj', async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const personajeID = req.params.idPj;
+    console.log(req.params);
+    const query =
+      'UPDATE personajes SET nombrePj = ?, apellido = ?, cumpleaños = ?, ocupacion = ?, boggart = ?, fkCasa = ?  WHERE idPersonaje= ?;';
+    const [results] = await connection.query(query, [
+      req.body.nombrePj,
+      req.body.apellido,
+      req.body.cumpleaños,
+      req.body.ocupacion,
+      req.body.boggart,
+      req.body.fkCasa,
+      personajeID,
+    ]);
+    connection.end();
+    res.json({
+      success: true,
+      message: 'Se ha podido modificar directamente la información de tu bruja',
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message:
+        'Compruba que estás cambiando todos los campos del la maga que quieras editar o inténtalo de nuevo más tarde',
+    });
+  }
+});
+
+//Borrar un personaje
+server.delete('/personajes/:idPj', async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const personajeID = req.params.idPj;
+
+    const query = 'DELETE FROM personajes WHERE idPersonaje = ?';
+    const [results] = await connection.query(query, [personajeID]);
+    connection.end();
+    console.log(results);
+    res.json({
+      success: true,
+      menssage: 'Se ha podido borrar correctamente a la bruja elejida.',
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message:
+        'No hemos podido borrar a tu maga, perdón. Inténtalo de nuevo más tarde',
+    });
+  }
+});
