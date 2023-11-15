78b4ba2420153876d0dcf3912af30f57f40c9d7f
Mayte <mayte710@hotmail.com>
2023-11-15 11:45:19 +0000

    readme

--- README.md
+++ README.md
@@ -1,5 +1,59 @@
-# modulo-4-evaluacion-final-MayteGonz
-modulo-4-evaluacion-final-MayteGonz created by GitHub Classroom
+# API de Personajes de Harry Potter
 
-Explicación de para qué es la api y que endpoints tenemos. 
-Foto de la relación entre las tablas.
+Bienvenido a la API de Personajes de Harry Potter. Esta API proporciona información sobre personajes, casas y usuarios del mundo mágico de Harry Potter.
+## Requisitos Previos
+- [Node.js](https://nodejs.org/)
+- [MySQL](https://www.mysql.com/)
+
+## Configuración del Proyecto
+1. Clona este repositorio: `git clone <(https://github.com/Adalab/modulo-4-evaluacion-final-MayteGonz.git)>`
+2. Instala las dependencias: `npm install`
+3. Configura las variables de entorno en un archivo `.env`, tienes en bd el archivo para crearte tu base de datos.
+4. Instala las demás dependecias con: `npm i`
+5. Ejecuta el servidor: `npm run dev`
+
+¡Listo! La API debería estar funcionando en [http://localhost:4001](http://localhost:4001).
+
+
+
+
+## Endpoints Disponibles
+
+### Obtener todos los personajes
+GET `/personajes`
+
+### Obtener todas las casas
+GET `/casas`
+
+### Obtener relación entre personajes y casas
+
+GET `/`
+
+### Añadir un nuevo personaje
+
+POST `/personajes`
+
+Permite añadir un nuevo personaje a la base de datos. Se deben proporcionar los datos del personaje en el cuerpo de la solicitud.
+
+### Modificar un personaje existente
+PUT `/personajes/:idPj`
+
+Permite modificar la información de un personaje existente. Se debe proporcionar el ID del personaje en la URL y los datos actualizados en el cuerpo de la solicitud
+
+### Borrar un personaje
+
+DELETE `/personajes/:idPj`
+
+Permite borrar un personaje existente. Se debe proporcionar el ID del personaje en la URL.
+
+### Registro de usuario
+
+POST `/registro`
+
+Permite registrar un nuevo usuario en la plataforma. Se deben proporcionar el correo electrónico, nombre de usuario y contraseña en el cuerpo de la solicitud.
+
+### Inicio de sesión de usuario
+
+POST `/login`
+
+Permite que un usuario registrado inicie sesión. Se deben proporcionar el correo electrónico y la contraseña en el cuerpo de la solicitud.
--- bd/bd.sql
+++ bd/bd.sql
@@ -59,7 +59,7 @@ CREATE TABLE `personajes` (
   `boggart` varchar(50) DEFAULT NULL,
   `fkCasa` int NOT NULL,
   PRIMARY KEY (`idPersonaje`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +71,33 @@ LOCK TABLES `personajes` WRITE;
 INSERT INTO `personajes` VALUES (1,'Hermione','Granger','1979-09-19','Estudiante','Profesora McGonagall diciendole que ha suspendido ',2),(2,'Ginevra','Weasley','1981-08-11','Estudiante','Ser controlada por Lord Voldemord',2),(3,'Nymphadora','Tonks','1973-08-23','Aurora','Perder sus habilidades',1),(4,'Pomona','Sprout','1942-05-15','Profesora','Lord Voldemort',1),(5,'Bellatrix','Lestrange','1951-05-02','Mortífaga','Voldemort rechazandola',4),(6,'Narcisa','Malfoy','1955-01-03','Ama de casa','Perder a su hijo',4),(7,'Luna','Lovegood','1981-02-13','Estudiante','Ver morir a su padre',3),(8,'Cho','Chang','1979-09-01','Estudiante','Ver a Cedric recriminandola ',3);
 /*!40000 ALTER TABLE `personajes` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `nombreUser` varchar(50) DEFAULT NULL,
+  `password` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (4,'pepi@gmail.com','pepi','$2b$10$/s4JgegZH7il6uZLpIH6puK0nsB/yAv7sT4piMPsnhDX7Mkq0iW2m'),(5,'',NULL,NULL);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +108,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-14 16:16:46
+-- Dump completed on 2023-11-15 11:28:52
